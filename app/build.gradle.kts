427cdd1736672e80613d5eeca50388fc573bd49e
Magnus V.Norling <68356591+manezz@users.noreply.github.com>
2024-05-14 09:43:49 +0200

    added api url

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,6 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     // https://mvnrepository.com/artifact/com.squareup.retrofit2/retrofit
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    // https://mvnrepository.com/artifact/com.google.code.gson/gson
-    implementation("com.google.code.gson:gson:2.10.1")
-
+    // https://mvnrepository.com/artifact/com.squareup.retrofit2/converter-gson
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 }
--- app/src/main/java/com/example/javaweatherapph5/ServiceBuilder.java
+++ app/src/main/java/com/example/javaweatherapph5/ServiceBuilder.java
@@ -1,15 +1,20 @@
 package com.example.javaweatherapph5;
 
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ServiceBuilder {
-    private static final String URL = "";
-//    private static Retrofit retrofit =
-//            new Retrofit.Builder().baseUrl(URL)
-//                    .addConverterFactory(GsonConverterFactory.create()).build();
-//    public static <F> F buildService(Class<F> serviceType)
-//    {
-//        return retrofit.create(serviceType);
-//    }
+    static String lat = "";
+    static String lon = "";
+    private static final String URL = "api.openweathermap.org/data/2.5/forecast?lat="
+            + lat +"&lon="+ lon +"&appid="
+            + System.getenv("API_EnvKey_JavaWeatherAppH5");
+    private static Retrofit retrofit =
+            new Retrofit.Builder().baseUrl(URL)
+                    .addConverterFactory(GsonConverterFactory.create()).build();
+    public static <F> F buildService(Class<F> serviceType)
+    {
+        return retrofit.create(serviceType);
+    }
 
 }
