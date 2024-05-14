70cd77cd4ae5e31c7d30b6f62a32859dec11e5ce
Marcus R.V <79831969+DoviihHawk@users.noreply.github.com>
2024-05-14 13:54:54 +0200

    fix

--- app/src/main/java/com/example/javaweatherapph5/models/WeatherItem.java
+++ app/src/main/java/com/example/javaweatherapph5/models/WeatherItem.java
@@ -12,46 +12,6 @@ Root root = om.readValue(myJsonString, Root.class); */
 @Data
 public class WeatherItem{
 
-    public String getCod() {
-        return cod;
-    }
-
-    public void setCod(String cod) {
-        this.cod = cod;
-    }
-
-    public int getMessage() {
-        return message;
-    }
-
-    public void setMessage(int message) {
-        this.message = message;
-    }
-
-    public int getCnt() {
-        return cnt;
-    }
-
-    public void setCnt(int cnt) {
-        this.cnt = cnt;
-    }
-
-    public ArrayList<List> getList() {
-        return list;
-    }
-
-    public void setList(ArrayList<List> list) {
-        this.list = list;
-    }
-
-    public City getCity() {
-        return city;
-    }
-
-    public void setCity(City city) {
-        this.city = city;
-    }
-
     public String cod;
     public int message;
     public int cnt;
